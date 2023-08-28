ae2b2c3b8c969f2c3ec225c29a27a6fd4ad69ebd
hp <hp@RAVIBAGHEL>
2023-08-28 15:24:08 +0530

    Add project files.

---
+++ ravi/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ravi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int a = 10; ;
+            int ab = 10; ;
+            int c;
+            c = a + ab;
+
+            Console.WriteLine(c);
+            Console.Write("hellow gla");
+            Console.ReadLine();
+        }
+    }
+}
